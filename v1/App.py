8c9a792f02cf0a4982ec232a7c32dca4ffdf8896
Lucas S. Brites <lucasbrites076@gmail.com>
2024-04-19 16:52:23 -0300

    Dando update na feature de INPUT-VALIDATION

--- v1/App.py
+++ v1/App.py
@@ -7,9 +7,8 @@ def cadastrarProduto(ID, nome, descricao, CP, CF, CV, IV, ML, CATEGORIA) -> Prod
     if VALIDAR.__(produto) not in[None, True]:
         print("ERRO PRODUTO")
         return None
+    produto.TABELA()
     return produto
 
 teste1 = cadastrarProduto(1022, "Testando Produto", "TESTE", 36, 15, 5, 12, 20, "TESTE")
-teste1.TABELA()
-
 
